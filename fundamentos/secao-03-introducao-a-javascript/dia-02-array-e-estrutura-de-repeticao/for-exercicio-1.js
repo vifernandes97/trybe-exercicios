0e012c1e691f8c1646c6bfa1d4d403c42974ba88
Vinicius Fernandes <vi.fernandes97@gmail.com>
2023-06-01 22:50:15 -0300

    exercicio 1 loop for

---
+++ fundamentos/secao-03-introducao-a-javascript/dia-02-array-e-estrutura-de-repeticao/for-exercicio-1.js
@@ -0,0 +1,6 @@
+const groceryList = ['Arroz', 'Feijão', 'Alface', 'Melancia', 'Macarrão'];
+
+for (index = 0; index < groceryList.length; index += 1) {
+let list = groceryList
+console.log(list[index]);
+}
