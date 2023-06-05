6b9635675e79145fe2e6a572e1d56059c07857bc
Vinicius Fernandes <vi.fernandes97@gmail.com>
2023-06-05 12:38:38 -0300

    adicionando template literals

---
+++ fundamentos/secao-03-introducao-a-javascript/template-literals.js
@@ -0,0 +1,7 @@
+/* O template literals é uma ferramenta do ES6 que permite criar strings complexas mais facilmente*/
+
+const myName = 'Isabella';
+
+console.log('Hello' + ' ' + myName + '!'); // concateneção
+
+console.log(`Welcome ${myName}!`); // template literals
