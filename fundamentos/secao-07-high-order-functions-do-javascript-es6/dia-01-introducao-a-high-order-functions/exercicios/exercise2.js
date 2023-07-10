2bdf4ac18a6f651303b36bdf53dddc92c70750f5
Vinicius Fernandes <vi.fernandes97@gmail.com>
2023-07-10 19:21:08 -0300

    requisitos 1 feito

---
+++ fundamentos/secao-07-high-order-functions-do-javascript-es6/dia-01-introducao-a-high-order-functions/exercicios/exercise2.js
@@ -0,0 +1,9 @@
+// retorne o nome do livro com o menor número de caracteres (menor nome)
+const expectedResult = 'Duna';
+const smallerName = () => {
+  let nameBook;
+  // escreva aqui o seu código
+
+  // Variável nameBook que receberá o valor do menor nome;
+  return nameBook;
+}
--- fundamentos/secao-07-high-order-functions-do-javascript-es6/dia-01-introducao-a-high-order-functions/exercicios/lotterySort.js
+++ fundamentos/secao-07-high-order-functions-do-javascript-es6/dia-01-introducao-a-high-order-functions/exercicios/lotterySort.js
@@ -1,16 +1,9 @@
-// Desenvolva uma HOF que retorne o resultado de um sorteio
+const verificaNumerosIguais = (meuNumero, numeroSorteado) => meuNumero === numeroSorteado;
 
+const sorteio = (meuNumero, verificaNumerosIguais) => {
+    const sorteiaNumero = Math.floor((Math.random() * 5) + 1);
 
-// funcao valida se os numeros sao iguais
-const numberChecker = (myNumber, number) => myNumber === number;
-
-// recebe um numero inputado e uma funcao como parametros
-const lotteryResult = (myNumber, callback) => {
-  const number = Math.floor((Math.random() * 5) + 1);
-
-  return callback(myNumber, number) ? 'Lucky day, you won!' : 'Try Again!';
+    return verificaNumerosIguais(meuNumero, sorteiaNumero) ? 'Você ganhou' : 'Tente novamente!';
 };
 
-console.log(lotteryResult(2, numberChecker));
-   
-
+console.log(sorteio(3, verificaNumerosIguais))
